cf5d15629b40fb063898f96a4211de45f198d5b6
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-06-11 20:45:30 +0400

    chore: Configure eslint, prettier, tsconfig

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,17 +4,45 @@ module.exports = {
     es2020: true,
     node: true,
   },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'prettier',
-  ],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: 11,
+    ecmaVersion: 13,
   },
   plugins: ['@typescript-eslint'],
+  root: true,
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
+    '@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/naming-convention': [
+      1,
+      {
+        selector: 'variable',
+        format: ['StrictPascalCase'],
+        prefix: ['are', 'can', 'did', 'does', 'has', 'have', 'is', 'should', 'will'],
+        types: ['boolean'],
+      },
+      {
+        selector: 'typeAlias',
+        format: ['StrictPascalCase'],
+        prefix: ['T'],
+      },
+    ],
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-empty-interface': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    'arrow-parens': 1,
+    'max-params': ['error', 3],
+    'no-duplicate-imports': 1,
+    'no-restricted-syntax': [
+      1,
+      {
+        selector: 'ExportDefaultDeclaration',
+        message: 'Prefer named exports.',
+      },
+    ],
+    'no-tabs': 1,
   },
-}
+};
