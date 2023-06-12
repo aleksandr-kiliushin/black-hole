10d2a60805740dc963efdb3a5a111940b5cfe7a4
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-06-12 23:48:00 +0400

    refactor: Inline ErrorBoundary content

--- packages/client/src/components/ErrorBoundary/ErrorBoundary.tsx
+++ packages/client/src/components/ErrorBoundary/ErrorBoundary.tsx
@@ -1,7 +1,5 @@
 import { Component, ErrorInfo, PropsWithChildren } from 'react';
 
-import { UnexpectedError } from './UnexpectedError';
-
 export class ErrorBoundary extends Component<PropsWithChildren, { hasError: boolean }> {
   state = { hasError: false };
 
@@ -15,7 +13,18 @@ export class ErrorBoundary extends Component<PropsWithChildren, { hasError: bool
 
   render() {
     if (this.state.hasError) {
-      return <UnexpectedError />;
+      return (
+        <div className="h-screen flex justify-center items-center">
+          <div className="flex flex-col justify-center items-center gap-8 border border-gray-100 p-5 shadow-lg">
+            <p className="text-lg font-medium leading-8 text-rose-500">
+              Произошла непредвиденная ошибка
+            </p>
+            <button className="ml-3 btn-primary" onClick={window.location.reload}>
+              Обновить страницу
+            </button>
+          </div>
+        </div>
+      );
     }
 
     return this.props.children;
