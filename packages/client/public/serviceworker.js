9362df2e1326b812fe108d4c553a4fa913f9c0b4
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-06-16 23:32:38 +0400

    feat: Use network-first strategy for caching

--- packages/client/public/serviceworker.js
+++ packages/client/public/serviceworker.js
@@ -3,22 +3,19 @@ const cacheName = 'black-hole-v1';
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     (async () => {
-      const responseFromCache = await caches.match(event.request);
-
-      if (responseFromCache !== undefined) {
-        return responseFromCache;
-      }
-
-      const cache = await caches.open(cacheName);
-
       try {
         const response = await fetch(event.request);
         if (response.status === 200) {
+          const cache = await caches.open(cacheName);
           cache.put(event.request, response.clone());
         }
         return response;
       } catch {
-        return responseFromCache;
+        const responseFromCache = await caches.match(event.request);
+        if (responseFromCache !== undefined) {
+          return responseFromCache;
+        }
+        throw new Error('Network request failed.');
       }
     })()
   );
