5a952a27bc9275fe6c25a98248cfb40eec2e7d90
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-07-06 11:39:10 +0400

    Restore useIsOnline hook

--- packages/client/src/utils/isOnline.ts
+++ packages/client/src/utils/isOnline.ts
@@ -1,19 +1,17 @@
 import { useEffect, useState } from 'react';
 
 export const useIsOnline = () => {
-  return { isOnline: true };
+  const [isOnline, setIsOnline] = useState(true);
 
-  // const [isOnline, setIsOnline] = useState(() => window.navigator.onLine);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setIsOnline(window.navigator.onLine);
+    }, 5000);
 
-  // useEffect(() => {
-  //   const intervalId = setInterval(() => {
-  //     setIsOnline(window.navigator.onLine);
-  //   }, 5000);
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
-  //   return () => {
-  //     clearInterval(intervalId);
-  //   };
-  // }, []);
-
-  // return { isOnline };
+  return { isOnline };
 };
