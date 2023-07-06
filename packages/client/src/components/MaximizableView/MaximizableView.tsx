65db28f59474fb7bb7ff70f6d1bf00b2f55575ca
Aleksandr Kiliushin <aleksandr.kiliushin@gmail.com>
2023-07-06 13:17:12 +0400

    Adapt useFullScreenStatus for SSR

--- packages/client/src/components/MaximizableView/MaximizableView.tsx
+++ packages/client/src/components/MaximizableView/MaximizableView.tsx
@@ -1,43 +1,42 @@
 import clsx from 'clsx';
 import { FC, PropsWithChildren, useRef } from 'react';
 
-// import { useFullscreenStatus } from '@utils/useFullscreenStatus';
+import { useFullscreenStatus } from '@utils/useFullscreenStatus';
+
 import { TMaximizableViewProps } from './types';
 
 export const MaximizableView: FC<PropsWithChildren<TMaximizableViewProps>> = ({
   children,
   backgroundColor,
 }) => {
-  return <>{children}</>;
+  const maximizableElement = useRef(null);
+  const { isFullscreen, setFullscreen, isFullscreenApiSupported } =
+    useFullscreenStatus(maximizableElement);
 
-  // const maximizableElement = useRef(null);
-  // const { isFullscreen, setFullscreen, isFullscreenApiSupported } =
-  //   useFullscreenStatus(maximizableElement);
-
-  // return (
-  //   <div
-  //     className={clsx(
-  //       'flex flex-row',
-  //       isFullscreen && 'flex-col-reverse content-end items-center pl-4'
-  //     )}
-  //     ref={maximizableElement}
-  //     style={{ backgroundColor: isFullscreen ? backgroundColor : '' }}
-  //   >
-  //     <div className="w-full h-full overflow-auto">{children}</div>
-  //     <div className="w-20 -ml-24 -mt-4">
-  //       {!isFullscreenApiSupported ? null : isFullscreen ? (
-  //         <button
-  //           className="btn btn-primary my-6 text-2xl absolute right-12"
-  //           onClick={() => document.exitFullscreen()}
-  //         >
-  //           ⛶
-  //         </button>
-  //       ) : (
-  //         <button className="btn btn-primary my-6 text-2xl" onClick={setFullscreen}>
-  //           ⛶
-  //         </button>
-  //       )}
-  //     </div>
-  //   </div>
-  // );
+  return (
+    <div
+      className={clsx(
+        'flex flex-row',
+        isFullscreen && 'flex-col-reverse content-end items-center pl-4'
+      )}
+      ref={maximizableElement}
+      style={{ backgroundColor: isFullscreen ? backgroundColor : '' }}
+    >
+      <div className="w-full h-full overflow-auto">{children}</div>
+      <div className="w-20 -ml-24 -mt-4">
+        {!isFullscreenApiSupported ? null : isFullscreen ? (
+          <button
+            className="btn btn-primary my-6 text-2xl absolute right-12"
+            onClick={() => document.exitFullscreen()}
+          >
+            ⛶
+          </button>
+        ) : (
+          <button className="btn btn-primary my-6 text-2xl" onClick={setFullscreen}>
+            ⛶
+          </button>
+        )}
+      </div>
+    </div>
+  );
 };
--- packages/client/src/utils/useFullscreenStatus.ts
+++ packages/client/src/utils/useFullscreenStatus.ts
@@ -1,58 +1,54 @@
-import React from 'react';
+import { RefObject, useCallback, useLayoutEffect, useState } from 'react';
 
-import { TFsDocument, TFullScreenElementProp } from '@components/MaximizableView/types';
+const getDocumentFullscreenElement = () => {
+  if ('mozFullScreenElement' in document) {
+    return document.mozFullScreenElement;
+  }
 
-const doc: TFsDocument = document;
+  if ('msFullscreenElement' in document) {
+    return document.msFullscreenElement;
+  }
 
-export const useFullscreenStatus = (ref: React.RefObject<HTMLElement>) => {
-  const [isFullscreen, setIsFullscreen] = React.useState(false);
-  const isFullscreenApiSupported = createInitialIsFullscreenApiSupported();
+  if ('webkitFullscreenElement' in document) {
+    return document.webkitFullscreenElement;
+  }
 
-  const setFullscreen = React.useCallback(() => {
+  if ('fullscreenElement' in document) {
+    return document.fullscreenElement;
+  }
+
+  return undefined;
+};
+
+export const useFullscreenStatus = (ref: RefObject<HTMLElement>) => {
+  const [isFullscreen, setIsFullscreen] = useState(false);
+
+  const setFullscreen = useCallback(() => {
     if (ref.current === null) return;
 
     ref.current
       .requestFullscreen()
       .then(() => {
-        setIsFullscreen(doc[getBrowserFullscreenElementProp()] !== null);
+        setIsFullscreen(getDocumentFullscreenElement() !== null);
       })
       .catch(() => {
         setIsFullscreen(false);
       });
   }, [ref]);
 
-  React.useLayoutEffect(() => {
-    document.onfullscreenchange = () =>
-      setIsFullscreen(doc[getBrowserFullscreenElementProp()] !== null);
+  useLayoutEffect(() => {
+    document.onfullscreenchange = () => {
+      setIsFullscreen(getDocumentFullscreenElement() !== null);
+    };
 
     return () => {
       document.onfullscreenchange = null;
     };
   });
 
-  return { isFullscreen, setFullscreen, isFullscreenApiSupported };
+  return {
+    isFullscreen,
+    setFullscreen,
+    isFullscreenApiSupported: getDocumentFullscreenElement() !== undefined,
+  };
 };
-
-const createInitialIsFullscreenApiSupported = () => {
-  let isInitialApiSupported = true;
-  try {
-    doc[getBrowserFullscreenElementProp()];
-  } catch (e) {
-    isInitialApiSupported = false;
-  }
-  return isInitialApiSupported;
-};
-
-function getBrowserFullscreenElementProp(): TFullScreenElementProp {
-  if (typeof doc.fullscreenElement !== 'undefined') {
-    return 'fullscreenElement';
-  } else if (typeof doc.mozFullScreenElement !== 'undefined') {
-    return 'mozFullScreenElement';
-  } else if (typeof doc.msFullscreenElement !== 'undefined') {
-    return 'msFullscreenElement';
-  } else if (typeof doc.webkitFullscreenElement !== 'undefined') {
-    return 'webkitFullscreenElement';
-  } else {
-    throw new Error('fullscreenElement не поддерживается вашим браузером');
-  }
-}
