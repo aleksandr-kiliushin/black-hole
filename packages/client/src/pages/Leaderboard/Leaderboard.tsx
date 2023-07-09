67606515a41dcfb02faf5fbb86b1995449ab241e
ahikyoshi <115409269+ahikyoshi@users.noreply.github.com>
2023-07-09 13:17:23 +0300

    Add leaderboard

--- packages/client/src/pages/Leaderboard/Leaderboard.tsx
+++ packages/client/src/pages/Leaderboard/Leaderboard.tsx
@@ -1,53 +1,157 @@
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
 
-import { Header } from '@components/Header';
+import { leaderboardApi } from '@api/leaderboardApi/leaderboardApi';
 
-const people = [
-  {
-    name: 'Calvin Hawkins',
-    email: 'calvin.hawkins@example.com',
-    image:
-      'https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    score: 2000,
-  },
-  {
-    name: 'Kristen Ramos',
-    email: 'kristen.ramos@example.com',
-    image:
-      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    score: 1800,
-  },
-  {
-    name: 'Ted Fox',
-    email: 'ted.fox@example.com',
-    image:
-      'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    score: 1344,
-  },
-];
+import { randomAvatarPath } from '@utils/randomAvatarPath';
+
+import { TleaderboardItems } from './types';
 
 export const Leaderboard: FC = () => {
+  const [list, setList] = useState<TleaderboardItems[]>([]);
+  const [correctPage, setCorrectPage] = useState<number>(1);
+
+  useEffect(() => {
+    leaderboardApi
+      .getLeaderBoard({
+        ratingFieldName: 'score',
+        cursor: (correctPage - 1) * 10,
+        limit: 10,
+      })
+      .then((res) => setList(res.data));
+  }, [correctPage]);
+
   return (
     <section className="overlay page-container my-6">
-      <h1 className="font-extrabold text-4xl">Leaderboard</h1>
-      <ul className="divide-y divide-gray-200">
-        {people.map((person, i) => (
-          <li
-            className="py-4 px-5 w-full flex flex-col sm:grid sm:grid-cols-[min-content_40px_30%_1fr_1fr] items-center gap-x-5"
-            key={person.email}
-          >
-            <p>{i + 1}.&nbsp;</p>
-            <img alt="" className="h-10 w-10 rounded-full" src={person.image} />
-            <div>
-              <p className="text-sm mx-auto sm:m-0 w-fit font-medium text-blue-200">
-                {person.name}
+      <h1 className="mb-2 text-4xl font-black text-center md:text-start">Лидеры</h1>
+
+      <ul className="flex my-4 text-center text-sm">
+        <li className="w-1/12 flex items-center justify-center">№</li>
+        <li className="w-4/12 flex items-center justify-center">Никнейм</li>
+        <li className="w-3/12 flex items-center justify-center">Счёт</li>
+        <li className="w-4/12 flex justify-center items-center">Поглощено объектов</li>
+      </ul>
+
+      <ul>
+        {list.map(({ data: { userID, userDisplayName, score, consumedEnemies } }, place) => (
+          <li className="py-2 flex text-center text-sm odd:bg-white/20" key={userID}>
+            <p className="w-1/12 flex items-center justify-center">{place + 1}</p>
+
+            <div className="w-4/12 flex items-center justify-center">
+              <img alt="" className="h-10 w-10 rounded-full" src={randomAvatarPath} />
+
+              <p className="ml-2 w-5/12 text-sm font-medium text-blue-200">
+                {userDisplayName} id:{userID}
               </p>
-              <p className="text-sm text-blue-400">{person.email}</p>
             </div>
-            <p className="mx-auto">Очки: {person.score}</p>
-            <button className="btn btn-primary w-1/2 sm:w-full sm:ml-auto">Like</button>
+
+            <p className="w-3/12 flex items-center justify-center">{score}</p>
+
+            <p className="w-4/12 flex items-center justify-center">{consumedEnemies}</p>
           </li>
         ))}
+      </ul>
+
+      <ul className="flex mt-2">
+        <li
+          className={
+            1 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(1)}
+        >
+          1
+        </li>
+        <li
+          className={
+            2 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(2)}
+        >
+          2
+        </li>
+        <li
+          className={
+            3 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(3)}
+        >
+          3
+        </li>
+        <li
+          className={
+            4 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(4)}
+        >
+          4
+        </li>
+        <li
+          className={
+            5 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(5)}
+        >
+          5
+        </li>
+        <li
+          className={
+            6 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(6)}
+        >
+          6
+        </li>
+        <li
+          className={
+            7 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(7)}
+        >
+          7
+        </li>
+        <li
+          className={
+            8 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(8)}
+        >
+          8
+        </li>
+        <li
+          className={
+            9 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(9)}
+        >
+          9
+        </li>
+        <li
+          className={
+            10 === correctPage
+              ? 'p-2 mr-1 bg-white/50 rounded-sm'
+              : 'p-2 mr-1 cursor-pointer rounded-sm hover:bg-white/30'
+          }
+          onClick={() => setCorrectPage(10)}
+        >
+          10
+        </li>
       </ul>
     </section>
   );
---
+++ packages/client/src/pages/Leaderboard/types.ts
@@ -0,0 +1,8 @@
+export type TLeaderboardItems = {
+  data: {
+    userID: number;
+    userDisplayName: string;
+    score: string;
+    consumedEnemies: number;
+  };
+};
