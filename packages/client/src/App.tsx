ad41fc901b259c1c88e330d7f75327735402fcb6
Kirill <qtrixnet@yandex.ru>
2023-07-10 22:29:51 +0600

    feat(JUS-50): remove styles import

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,5 @@
 import clsx from 'clsx';
 import { FC, useCallback, useEffect } from 'react';
-import 'react-toastify/dist/ReactToastify.css';
 
 import { authActions, getAuthUserInfo } from '@store/slices/auth/authSlice';
 
